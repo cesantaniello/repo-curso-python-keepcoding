234d2940fc7f90f57ee27bf634fc5904e0411dd5
Carlos <carlos.e.santaniello@gmail.com>
2019-08-14 21:56:35 +0200

    Creacion de nuevos objetos con sus metodos y atributos

--- objetos.py
+++ objetos.py
@@ -1,11 +1,17 @@
 import turtle
 
 tortuguita = turtle.Turtle()
-otraTortuguita = turtle.Turtle() 
+otraTortuguita = turtle.Turtle()
+lentorra = turtle.Turtle()
+
+lentorra.speed(1)
 
 tortuguita.shape('turtle')
+tortuguita.color('blue')
 tortuguita.fd(50)
+tortuguita.speed(5)
 
 otraTortuguita.color('green')
 otraTortuguita.left(90)
 otraTortuguita.fd(50)
+otraTortuguita.speed(5)
