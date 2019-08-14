c87def89beb96026d7d5ad98150ce569810bb0ec
Carlos <carlos.e.santaniello@gmail.com>
2019-08-14 22:39:55 +0200

    Creacion de nuevos objetos

--- perro.py
+++ perro.py
@@ -4,3 +4,19 @@ class Perro():
         self.nombre = nombre
         self.edad = edad
         self.peso = peso
+     
+    #creo la funcion ladrar 
+    def ladrar(self):
+        if self.peso >= 8:
+            print("GUAU, GUAU")
+        else:
+            print("Guau, guau")
+            
+salchicho = Perro("Salchicho", 3, 12)
+lola = Perro("Lola", 8, 1.5)
+miko = Perro("Miko", 8, 3)
+
+salchicho.ladrar()
+lola.ladrar()
+        
+        
