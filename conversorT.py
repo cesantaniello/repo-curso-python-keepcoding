0bef9d4d5650f0bdc97f0f7b671391fe42e63fba
Carlos <carlos.e.santaniello@gmail.com>
2019-08-16 21:28:24 +0200

    Actualizacion de termometro

--- conversorT.py
+++ conversorT.py
@@ -3,6 +3,24 @@ class Termometro():
         self.__unidadM = 'C'
         self.__temperatura = 0
         
+    def __str__(self):
+        return "{}º {}".format(self.__temperatura, self.__unidadM)
+    
+    #Getter y Setter de unidadMedida
+    def unidadMedida(self, uniM=None):
+        if uniM == None:
+            return self.__unidadM
+        else:
+            if uniM == 'F' or uniM == 'C':
+                self.__unidadM = uniM
+                
+    #Getter y Setter de temp            
+    def temp(self, temperatura = None):
+        if temperatura == None:
+            return self.__temperatura
+        else:
+            self.__temperatura = temperatura
+            
     def __conversor(self, temperatura, unidad):
         if unidad == 'C':
             return "{}º F".format(temperatura * 9/5 + 32)
@@ -11,26 +29,11 @@ class Termometro():
         else:
             return "Unidad incorrecta"
         
-    def __str__(self):
-        return "{}º {}".format(self.__temperatura, self.__unidadM)
-    
-    #Getter y Setters de unidadMedida
-    def unidadMedida(self, uniM=None):
-        if uniM == None:
-            return self.__unidadM
-        else:
-            if uniM == 'F' or uniM == 'C':
-                self.__unidadM = uniM
-                
-    #Getter y Setters de temp            
-    def temp(self, temperatura = None):
-        if temperatura == None:
-            return self.__temperatura
-        else:
-            self.__temperatura = temperatura
-            
     def mide(self, uniM = None):
         if uniM == None or uniM == self.__unidadM:
             return self.__str__()
         else:
-            return self.__conversor(self.__temperatura, self.__unidadM)
+            if uniM == 'F' or uniM == 'C':
+                return self.__conversor(self.__temperatura, self.__unidadM)
+            else:
+                return self.__str__()
